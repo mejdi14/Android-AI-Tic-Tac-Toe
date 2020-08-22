25d79f6c6d921cf195b94d78dfd09587e5562942
ubuntu <mejdiclubiste@gmail.com>
2020-08-22 21:11:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/ai_tic_tac_toe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ai_tic_tac_toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
