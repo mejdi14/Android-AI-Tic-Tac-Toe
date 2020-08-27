0c79dc28e75e1b4838637d9e1c494d2a4f911e6d
ubuntu <mejdiclubiste@gmail.com>
2020-08-27 01:44:00 +0100

    add click listeners

--- app/src/main/java/com/example/ai_tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/ai_tic_tac_toe/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.ai_tic_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        attachClickListener()
+    }
+
+    private fun attachClickListener() {
+        img1.setOnClickListener { this }
+        img2.setOnClickListener { this }
+        img3.setOnClickListener { this }
+        img4.setOnClickListener { this }
+        img5.setOnClickListener { this }
+        img6.setOnClickListener { this }
+        img7.setOnClickListener { this }
+        img8.setOnClickListener { this }
+        img9.setOnClickListener { this }
+    }
+
+    override fun onClick(p0: View?) {
+        Log.d("clicked item", "onClick: ")
     }
 }
