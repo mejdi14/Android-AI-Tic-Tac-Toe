a54dff3dc26df6993c08a8b4dfd29defebbedfe3
unknown <mejdiclubiste@gmail.com>
2020-09-11 14:38:09 -0700

    listener is listening

--- app/src/main/java/com/example/ai_tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/ai_tic_tac_toe/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
@@ -14,18 +15,19 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     private fun attachClickListener() {
-        img1.setOnClickListener { this }
-        img2.setOnClickListener { this }
-        img3.setOnClickListener { this }
-        img4.setOnClickListener { this }
-        img5.setOnClickListener { this }
-        img6.setOnClickListener { this }
-        img7.setOnClickListener { this }
-        img8.setOnClickListener { this }
-        img9.setOnClickListener { this }
+        img1.setOnClickListener(this)
+        img2.setOnClickListener(this)
+        img3.setOnClickListener(this)
+        img4.setOnClickListener(this)
+        img5.setOnClickListener(this)
+        img6.setOnClickListener(this)
+        img7.setOnClickListener(this)
+        img8.setOnClickListener(this)
+        img9.setOnClickListener(this)
     }
 
-    override fun onClick(p0: View?) {
-        Log.d("clicked item", "onClick: ")
+    override fun onClick(view: View?) {
+        Log.d("this image", "onClick: "+ view)
+        (view as ImageView)?.setImageResource(R.drawable.cross)
     }
 }
